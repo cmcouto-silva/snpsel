833e348ce44660ca1066a73a09574035d9fccc3c
Cainã Max Couto-Silva <cmcouto.silva@gmail.com>
2018-04-10 16:26:38 -0300

    Add check_plink_version.R

---
+++ R/check_plink_version.R
@@ -0,0 +1,14 @@
+#' @export
+ 
+check_plink_version <- function(){
+  
+  plink_v <- gsub(".*v(\\S..).*", "\\1", system('plink --version', T))
+  
+  if (plink_v >= 1.9) {
+    plink_v <- "plink"
+  } else {
+    plink_v <- "plink --noweb"
+  }
+  return(plink_v)
+}
+
