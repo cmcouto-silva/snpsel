ed54d8ae97428711445429c206406d904e618180
Cainã Max Couto-Silva <cmcouto.silva@gmail.com>
2018-04-26 11:00:50 -0300

    split_chrs_haps()

--- R/split_chrs_haps.R
+++ R/split_chrs_haps.R
@@ -7,7 +7,9 @@ split_chrs_haps <- function(haps, output, prefx = T) {
   haps <- data.table::fread(paste0(haps, ".haps"))
   log <- missing(output)
 
-  file_name <- parse(text = 'ifelse(test = { prefx == T }, yes = paste0("chr", i, if(!log) {paste0("_", output)}), no = paste0(if(!log) {paste0(output, "_")}, "chr", i))')
+  file_name <- parse(text = paste('ifelse(test = { prefx == T },',
+                     'yes = paste0("chr", i, if(!log) {paste0("_", output)}),',
+                     'no = paste0(if(!log) {paste0(output, "_")}, "chr", i))'))
 
   for (i in unique(haps[, V1])){
     fwrite(x = haps[V1 == i], file = paste0(eval(file_name), ".haps"), quote = F, sep = " ", row.names = F, col.names = F)
@@ -16,4 +18,4 @@ split_chrs_haps <- function(haps, output, prefx = T) {
 }
 
 # Usage
-# split_chrs_haps(haps = "all_chrs_anc_adjusted", output = "phased")
+# split_chrs_haps(haps = "all_chrs_anc_adjusted", output = "adjusted")
