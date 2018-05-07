14380747c3c0228bbbebb3219a8961ff2389e163
cmcouto-silva <cmcouto.silva@gmail.com>
2018-05-07 15:47:50 -0300

    update pkg.lib()

--- R/pkg.lib.R
+++ R/pkg.lib.R
@@ -1,20 +1,29 @@
 #' @title Load multiple packages in R
-#' @description This function loads multiple packages according to a vector of names, and install them if they're not already installed. Only packages avaialable on CRAN will be installed.
-#' @param packages A vector with package names.
+#' @description This function loads multiple packages according to their names, and install them if they're not already installed.
+#' @param ... Package names (with or without double quotes)
+#' @details Only packages available on CRAN's repository will be automatically installed using this functions.
+#' For packages stored in another repository, please install them separately according to proper instructions.
 #' @return Packages attached into namespace.
 #' @examples
 #' \dontrun{
-#' required_packages <- c("tidyverse", "dplyr", "lubridate")
-#' pkg.lib(packages = required_packages)
+#' pkg.lib(data.table, dplyr, knitr)
 #' }
 #' @export
 
 pkg.lib <- function(...) {
   pkgs <- as.character(sys.call(0))[-1]
+
   sapply(pkgs, function(pkg){
-    if(!requireNamespace(pkg)) install.packages(pkg, dependencies = TRUE)
+    if(!requireNamespace(pkg, quietly = T)) {
+      install.packages(pkg, dependencies = TRUE, quiet = T)
+    }
+
+    tryCatch({
     library(pkg, character.only = TRUE)
     cat(paste0('  Package "', pkg, '" successfully loaded!\n'))
-  })
+    }, error = function(e) cat(paste0("  unable to load ‘", pkg, "’ package\n")))
+    })
+
   return(invisible())
 }
+
