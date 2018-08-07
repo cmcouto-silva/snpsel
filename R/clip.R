f3068f466a42341057d141f724035c719da3aa24
cmcouto-silva <cmcouto.silva@gmail.com>
2018-08-07 11:34:30 -0300

    xclip

--- R/clip.R
+++ R/clip.R
@@ -1,14 +1,14 @@
 #' @title Remove additional spaces/paragraphs from clipboard text
 #' @description This function removes any additional whitespace or paragraphs from copied text (clipboard)
 #' in a single block of text, adding it to the clipboard OS
-#' 
+#'
 #' @return Single block text in the clipboard.
 #' @export
-#' 
+#'
 #' @author Cainã Max Couto-Silva
 
-clip <- function(){
-  x <- tm::stripWhitespace(clipr::read_clip()) 
+xclip <- function(){
+  x <- tm::stripWhitespace(clipr::read_clip())
   x <- paste(x, collapse = " ")
   clipr::write_clip(x)
 }
