a87c90f7f1c0445f6e84fefb7ffb27a2a887facf
cmcouto-silva <cmcouto.silva@gmail.com>
2018-08-07 09:56:18 -0300

    up clip function

--- R/check_plink_version.R
+++ R/check_plink_version.R
@@ -1,9 +1,9 @@
 #' @export
- 
+
 check_plink_version <- function(){
-  
+
   plink_v <- gsub(".*v(\\S..).*", "\\1", system('plink --version', T))
-  
+
   if (plink_v >= 1.9) {
     plink_v <- "plink"
   } else {
