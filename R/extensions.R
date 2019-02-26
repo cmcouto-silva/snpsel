e449f78e400233bb692dec1ee54622e4655219ab
cmcouto-silva <cmcouto.silva@gmail.com>
2019-02-26 17:47:55 -0300

    update extensions.R

--- R/extensions.R
+++ R/extensions.R
@@ -8,18 +8,21 @@ substr_last <- function(x, digits){
 
 # Checking extension existence
 check.extension <- function(x, digits = 3L) {
-  grepl(pattern <- paste0("^.*\\..{", digits, "}$"), x, perl = T)
+  grepl(pattern = paste0("^.*\\..{", digits, "}$"), x = x, perl = T)
 }
 
 #' @export
 # Adding extension
 add.extension <- function(x, ext, digits = 3L) {
-  ext <- ifelse(test = grepl("^\\.", ext), ext, paste0('.', ext))
-  if(substr_last(x, digits) == substr_last(ext, 3L)) {
-    return(x)
-  } else {
-    return(paste0(x, ext))
-  }
+  sapply(x, function(x) {
+    ext <- ifelse(test = grepl("^\\.", ext), ext, paste0('.', ext))
+    if(substr_last(x, digits) == substr_last(ext, 3L)) {
+      return(x)
+    } else {
+      return(paste0(x, ext))
+    }
+  })
+
 }
 
 #' @title Remove file extension
@@ -32,14 +35,18 @@ add.extension <- function(x, ext, digits = 3L) {
 
 rm.extension <- function(file, digits = 3L) {
   
-  if(!grepl("\\.", file)) stop("There is no extension in this file.")
+  sapply(file, function(file) {
+    if(!grepl("\\.", file)) stop("There is no extension in this file.")
+    
+    ext.digits <- unlist(strsplit(file, "\\."))
+    ext.digits <- ext.digits[length(ext.digits)]
+    
+    if (nchar(ext.digits) != digits)
+      stop("File extension (size = ", nchar(ext.digits), ") after dot differs from ", digits, " digits.")
+    
+    pat <- paste0("^(.*)\\..{", digits, "}$")
+    res <- gsub(pat, "\\1", file)
+    return(res)
+  })
   
-  ext.digits <- unlist(strsplit(file, "\\."))
-  ext.digits <- ext.digits[length(ext.digits)]
-  
-  if (nchar(ext.digits) != digits)
-    stop("File extension (size = ", nchar(ext.digits), ") after dot differs from ", digits, " digits.")
-  
-  pat <- paste0("^(.*)\\..{", digits, "}$")
-  gsub(pat, "\\1", file)
 }
