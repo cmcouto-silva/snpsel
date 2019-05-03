713b3bad75212aaa42cc278100161f044df6a9ff
cmcouto-silva <cmcouto.silva@gmail.com>
2019-05-03 19:00:26 -0300

    update plink_merge.R

--- R/plink_merge.R
+++ R/plink_merge.R
@@ -5,8 +5,8 @@
 plink_merge <- function(dataset01, dataset02, out, keep.snp.names = 1L) {
   
   # Loading datasets
-  ds1 <- gt::read.bim(paste0(dataset01, '.bim'))
-  ds2 <- gt::read.bim(paste0(dataset02, '.bim'))
+  ds1 <- read.bim(paste0(dataset01, '.bim'))
+  ds2 <- read.bim(paste0(dataset02, '.bim'))
   
   # Verify matching positions
   ds_match <- merge(ds1, ds2, by = c("CHR","POS"), sort = F)
@@ -16,12 +16,10 @@ plink_merge <- function(dataset01, dataset02, out, keep.snp.names = 1L) {
   writeLines(ds_match[, SNP.y], paste0(dataset02, '_Rtmp.txt'))
   
   # Running Plink for removing non-match SNPs in each dataset
-  plink <- plink_version()
-  
   for (ds in c(dataset01, dataset02)) {
-    system(paste(
-      plink, "--bfile", ds, "--extract", paste0(ds, '_Rtmp.txt'), "--make-bed", "--out", paste0(ds, '_Rtmp')
-    )); unlink(paste0(ds, '_Rtmp.txt'))
+    plink(`--bfile` = ds, `--extract` = paste0(ds, '_Rtmp.txt'), "--make-bed", "--allow-no-sex",
+          `--out` = paste0(ds, '_Rtmp'))
+    unlink(paste0(ds, '_Rtmp.txt'))
   }
   
   # Loading datasets with matching positions
@@ -38,11 +36,9 @@ plink_merge <- function(dataset01, dataset02, out, keep.snp.names = 1L) {
   }
   
   # Merge datasets
-  system(paste(
-    plink, "--bfile", paste0(dataset01, '_Rtmp'),
-    "--bmerge", paste0(paste0(dataset02, '_Rtmp'), c('.bed','.bim','.fam'), collapse = " "), 
-    "--make-bed", "--out", out
-  ))
+  plink(`--bfile` = paste0(dataset01, '_Rtmp'),
+        `--bmerge` = paste0(paste0(dataset02, '_Rtmp'), c('.bed','.bim','.fam'), collapse = " "), 
+        "--make-bed", `--out` = out)
   
   files.to.remove <- c (
     paste0(out, ".log"),
@@ -52,36 +48,30 @@ plink_merge <- function(dataset01, dataset02, out, keep.snp.names = 1L) {
   
   # Dealing with problematic SNPs (flipping strand and removing triallelic+ SNPs )
   
-  if (file.exists(paste0(out, "-merge.missnp"))) {
+  if(file.exists(paste0(out, "-merge.missnp"))) {
     file.rename(paste0(out, "-merge.missnp"), paste0(out, "-merge_Rtmp.missnp"))
     
     # Flipping
-    system(paste(
-      plink, "--bfile", paste0(dataset01, '_Rtmp'), "--flip", paste0(out, "-merge_Rtmp.missnp"),
-      "--make-bed", "--out", paste0(dataset01, '_Rtmp')
-    ))
+    plink(`--bfile` = paste0(dataset01, '_Rtmp'), `--flip` = paste0(out, "-merge_Rtmp.missnp"),
+      "--make-bed", `--out` = paste0(dataset01, '_Rtmp'))
     
     # Trying to merge
-    system(paste(
-      plink, "--bfile", paste0(dataset01, '_Rtmp'),
-      "--bmerge", paste0(paste0(dataset02, '_Rtmp'), c('.bed','.bim','.fam'), collapse = " "), 
-      "--make-bed", "--out", out
-    ))
+    plink(`--bfile` = paste0(dataset01, '_Rtmp'),
+          `--bmerge` = paste0(paste0(dataset02, '_Rtmp'), c('.bed','.bim','.fam'), collapse = " "),
+          "--make-bed", "--allow-no-sex", `--out` = out)
     
     if(file.exists(paste0(out, "-merge.missnp"))) {
       
+      # Remove problematic SNPs
       for (ds in c(paste0(dataset01, '_Rtmp'), paste0(dataset02, '_Rtmp'))) {
-        system(paste(
-          plink, "--bfile", ds, "--exclude", paste0(out, "-merge.missnp"), "--make-bed", "--out", ds ))
-        
+        plink(`--bfile` = ds, `--exclude` = paste0(out, "-merge.missnp"), "--make-bed", "--allow-no-sex", `--out` = ds)
         files.to.remove <- append(files.to.remove, paste0(ds, paste0(c('.bed~','.bim~','.fam~'))))
       }
       
       # Final merge
-      system(paste(
-        plink, "--bfile", paste0(dataset01, '_Rtmp'),
-        "--bmerge", paste0(paste0(dataset02, '_Rtmp'), c('.bed','.bim','.fam'), collapse = " "), 
-        "--make-bed", "--out", out ))
+      plink(`--bfile` = paste0(dataset01, '_Rtmp'),
+            `--bmerge` = paste0(paste0(dataset02, '_Rtmp'), c('.bed','.bim','.fam'), collapse = " "),
+            "--make-bed", "--allow-no-sex", `--out` = out)
       
       files.to.remove <- append(files.to.remove, paste0(out, "-merge.missnp"))
     
@@ -89,8 +79,7 @@ plink_merge <- function(dataset01, dataset02, out, keep.snp.names = 1L) {
     
     files.to.remove <- append(files.to.remove, c(
       paste0(out, '-merge_Rtmp.missnp'),
-      paste0(dataset01, '*~')),
-      
+      paste0(dataset01, '*~'))
       )
     
   }
