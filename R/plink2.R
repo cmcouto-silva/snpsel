d3811b53f27dd395501f2feb8aa8fee8706d9216
cmcouto-silva <cmcouto.silva@gmail.com>
2019-07-10 17:15:02 -0300

    add and modify plink[2].R

--- R/plink2.R
+++ R/plink2.R
@@ -1,11 +1,11 @@
 #' @export
-plink <- function(...) {
+plink2 <- function(...) {
   values <- c(...)
   args <- names(values)
-  plink <- plink_version()
+  program_on_path("plink2")
   
   commands <- ifelse(args == "", values, paste(args, values))
   commands <- paste(commands, collapse = " ")
   
-  system(paste(plink, commands))
+  system(paste("plink2", commands))
 }
