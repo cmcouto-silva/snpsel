c1d28083637195d0e6bc678a630a96d1ac6f4bc3
cmcouto-silva <cmcouto.silva@gmail.com>
2020-09-14 10:44:40 -0300

    add function to annotate genes from pre-worked annovar table

---
+++ R/annotate_fromtbl.R
@@ -0,0 +1,52 @@
+#' @title Annotate genes from a reference table
+#' @description Given a reference tidy table with the columns "SNP", "GENE", "DIST", and "FUNCTION", 
+#' this function merges it with the dataset by SNP (so each SNP must be unique), and then concatenate
+#' the genes and their respective function by comma to set unique records.
+#' @param snp vector. A vector with SNP names to match the ref[["SNP"]] column.
+#' @param thres Integer. A single integer to set the maximum allowed distance (bp) from the annotated gene. 
+#' Default set to 10kb.
+#' @examples
+#' \dontrun{
+#' annotate_fromtbl(genes)
+#' dt[, c("GENE", "FUNCTION) := annotate_fromtbl(SNP)]
+#' }
+#' @import data.table
+#' @return vector of genes or list with two vectors (genes and functions)
+#' @author Cainã Max Couto-Silva
+#' @export
+
+annotate_fromtbl <- function (snp, ref, thres = 10000, include_function = T) {
+  
+  snp_dt <- data.table(SNP=snp)
+  ref <- fread(ref)
+  
+  if(!all(snp_dt[, SNP] %in% ref[, SNP])) {
+    stop("Not all SNPs from dataset annotated in ref file. Please fix the reference.")
+  }
+  
+  dt <- merge(snp_dt, ref[, .(SNP, GENE, DIST, FUNCTION)], by="SNP", sort = F)
+  dt[DIST > thres, c("GENE", "FUNCTION") := ""]
+  
+  dt <- dt[, lapply(.SD, function(x) {
+    if(length(x)==1) {
+      x
+    } else if (length(unique(x)) == 1 && unique(x) == "") {
+      ""
+    } else {
+      trimws(paste(x, collapse = ","), whitespace = "[,\t\r\n]")
+    }
+  }), by = SNP, .SDcols = c("GENE", "FUNCTION")]
+  
+  if(all(dt[, SNP] == snp)) {
+    stop("SNPs didn't keep the order. Please check the original function.")
+  }
+  
+  if(include_function) {
+    annot <- as.list(dt[, .(GENE, FUNCTION)])
+  } else {
+    annot <- dt[, GENE]
+  }
+  
+  return(annot)
+  
+}
--- R/get_snp_genes.R
+++ R/get_snp_genes.R
@@ -7,12 +7,12 @@
 #' @export
 
 get_snp_genes <- function(snp, ref = "~/cmcouto.silva@usp.br/lab_files/all_datasets/Reference_annotation", 
-                          include_function = F) {
+                          include = NULL) {
   ref <- data.table::fread(ref)
   snp <- data.table::data.table(SNP = snp)
   snp.genes <- merge(snp, ref, by = "SNP", sort = F, all.x = T)
-  if(include_function) {
-    snp.genes <- snp.genes[, .(GENE, FUNCTION)]
+  if(!is.null(include)) {
+    snp.genes <- snp.genes[, .SD, .SDcols = c("GENE", include)]
   } else {
     snp.genes <- snp.genes[, GENE]
   }
