93ab584f70fc9f5bb930a73f5576f4cfa12deb55
cmcouto-silva <cmcouto.silva@gmail.com>
2020-09-28 13:48:15 -0300

    add new function to subset samples in vcf file

---
+++ R/subset_vcf.R
@@ -0,0 +1,16 @@
+#' @export
+#' @author Cainã Max Couto-Silva
+
+subset_vcf <- function(vcf, ids, out) {
+  # Create temporary file to store list of individuals
+  tmp <- paste0(gsub(" ", "_", Sys.time()), "_tmp.txt")
+  writeLines(ids, tmp)
+  # Run Plink to remove them
+  plink2 (
+    `--vcf` = vcf, `--keep` = tmp,
+    `--export` = paste0("vcf bgz id-delim=", id.delim),
+    `--out` = out
+    )
+  # Remove temporary file
+  unlink(tmp)
+}
