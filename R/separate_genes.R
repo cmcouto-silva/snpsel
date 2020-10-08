9301fe1a59c9bac99e27eaba708e357899884cc3
cmcouto-silva <cmcouto.silva@gmail.com>
2020-10-07 22:33:05 -0300

    update function to match more target-columns

--- R/separate_genes.R
+++ R/separate_genes.R
@@ -3,39 +3,22 @@
 #' character. It's specifically created for using the update_dist_genes function, after annotating with 
 #' ANNOVAR.
 #' @param data A data.frame of data.table with the a column (default="GENE") submitted to update_dist_genes() function.
-#' @param GENE Character. Target-column name (still not well implemented)
-#' @param FUNCTION Character. Target=column to FUNCTION column to replace ";" by ",". Set it to NULL to disable
-#' this function.
+#' @param GENE Character vector. Target-column(s) name(s).
 #' @param setdt Logical. If TRUE (default), returns an object from the class "data.table".
 #' @return Exploded table with unique genes by row (observation) and duplicated values on the other columns.
 #' @author Cainã Max Couto-Silva
 #' @import data.table magrittr
 #' @export
 
-separate_genes <- function(data, GENE = "GENE", FUNCTION = "FUNCTION", keep_empty = FALSE, setdt = TRUE) {
+separate_genes <- function(data, gene_col="GENE", keep_empty = FALSE, setdt = TRUE) {
   
-  # unify_duplicates <- function(genes) {
-  #   sapply(strsplit(genes, split = ",|;"), function(g) {
-  #     ifelse(length(unique(g)) == 1, g[1], paste(g, collapse = ","))
-  #   })
-  # }
-  # 
-  # dt <- as.data.table(data)
-  # dt[grepl(",|;", GENE), GENE := unify_duplicates(GENE)]
-  # 
-  # if(!is.null(FUNCTION)) dt[grepl(";", FUNCTION), FUNCTION := gsub(";", ",", FUNCTION)]
-  # 
-  # df <- tidyr::separate_rows(dt, "GENE", sep = ",|;")
-  # if(setdt) df <- as.data.table(df)
-  # return(df)
-  # 
+  dt <- tidyr::separate_rows(data, all_of(gene_col), sep=",")
   
-  if(is.null(FUNCTION)) {
-    dt_exploded <- tidyr::separate_rows(data, GENE, sep=",")
-  } else {
-    dt_exploded <- tidyr::separate_rows(data, GENE, FUNCTION, sep=",")
+  if(isFALSE(keep_empty)) {
+    empty_idx <- apply(dt[, gene_col], 1, function(x) any(x=="" | is.na(x)))
+    dt <- dt[!empty_idx, ]
   }
-  if(isFALSE(keep_empty)) dt_exploded <- dt_exploded[dt_exploded[[GENE]]!="", ]
-  if(setdt) dt_exploded <- as.data.table(dt_exploded)
-  return(dt_exploded)
+  
+  if(setdt) dt <- as.data.table(dt)
+  return(dt)
 }
